f7293e8b3e11d32fa278a3e5f0f161ecbe6a8a14
shrutiso05 <shristisoni051199@gmail.com>
2023-11-21 20:38:29 +0530

    setup_urls

---
+++ authentication/urls.py
@@ -0,0 +1,8 @@
+# authentication/urls.py
+from django.urls import path
+from .views import signup, login
+
+urlpatterns = [
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
+]
