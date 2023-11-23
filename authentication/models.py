eb87030e444a77df80bd2f7c59d355cb3fdb0389
shrutiso05 <shristisoni051199@gmail.com>
2023-11-23 15:58:28 +0530

    setup_model_1

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     company_name = models.CharField(max_length=100)
@@ -8,6 +10,14 @@ class UserProfile(models.Model):
     business_email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
     business_email = models.EmailField(unique=True)
+ 
+    def __str__(self):
+        return self.username
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
-        return self.business_email
+        return self.name
+
+ 
